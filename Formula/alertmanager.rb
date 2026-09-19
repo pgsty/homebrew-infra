040c1b25857b8deecae68c1e6026b0966ea91ce7
Feng Ruohang <rh@vonng.com>
2026-09-19 07:25:55 +0000

    chore: update Homebrew formulae

--- Formula/alertmanager.rb
+++ Formula/alertmanager.rb
@@ -9,26 +9,26 @@ class Alertmanager < Formula
   on_macos do
     on_arm do
       # update: darwin_arm64
-      url "https://github.com/prometheus/alertmanager/releases/download/v0.34.0/alertmanager-0.34.0.darwin-arm64.tar.gz"
-      sha256 "0cb31efe439c58ab77594b62a28f9de95f2d08beccf0f11eba4b822b2f549b82"
+      url "https://github.com/prometheus/alertmanager/releases/download/v0.34.1/alertmanager-0.34.1.darwin-arm64.tar.gz"
+      sha256 "a3941879f340ef12a4cc1a479c82bd8520611265ea8d1ee25c88bd5f944d8293"
     end
     on_intel do
       # update: darwin_amd64
-      url "https://github.com/prometheus/alertmanager/releases/download/v0.34.0/alertmanager-0.34.0.darwin-amd64.tar.gz"
-      sha256 "2c76c6bb30f030296cbfbf803bfd99494987197fcb8743f73ef5f351d568ce97"
+      url "https://github.com/prometheus/alertmanager/releases/download/v0.34.1/alertmanager-0.34.1.darwin-amd64.tar.gz"
+      sha256 "9f3a3806c81a2ef3546fea9bae3dd39abcf2206edd112c9433133a9355875fce"
     end
   end
 
   on_linux do
     on_arm do
       # update: linux_arm64
-      url "https://github.com/prometheus/alertmanager/releases/download/v0.34.0/alertmanager-0.34.0.linux-arm64.tar.gz"
-      sha256 "a96ef16598ddc58e84d28167b3352b30e3205698d76c56ef467e4504d7664da4"
+      url "https://github.com/prometheus/alertmanager/releases/download/v0.34.1/alertmanager-0.34.1.linux-arm64.tar.gz"
+      sha256 "d98d6cbaf52151c7e76e24355fec88b11cebcb9875d4cdd8b76ddce7a7e5535c"
     end
     on_intel do
       # update: linux_amd64
-      url "https://github.com/prometheus/alertmanager/releases/download/v0.34.0/alertmanager-0.34.0.linux-amd64.tar.gz"
-      sha256 "19c75a11d8c03dc4ade7abdbddfb3a8f28c9e7b000d0849cda0cd71dffd74a03"
+      url "https://github.com/prometheus/alertmanager/releases/download/v0.34.1/alertmanager-0.34.1.linux-amd64.tar.gz"
+      sha256 "265b9d1e55ef0d5306a436018af6d2b686c2ce051f03d968f7464ecb1372a7e8"
     end
   end
 
--- Formula/mcli.rb
+++ Formula/mcli.rb
@@ -4,7 +4,7 @@
 class Mcli < Formula
   desc "Command-line client for Silo and S3-compatible object storage"
   homepage "https://silo.pgsty.com"
-  version "2026-09-03T07-13-05Z"
+  version "2026-09-16T00-00-00Z"
   license "AGPL-3.0-or-later"
 
   livecheck do
@@ -16,26 +16,26 @@ class Mcli < Formula
   on_macos do
     on_arm do
       # update: darwin_arm64
-      url "https://github.com/pgsty/mc/releases/download/RELEASE.2026-09-03T07-13-05Z/mcli_20260903071305.0.0_darwin_arm64.tar.gz"
-      sha256 "d1f889f23662aa488e70576014fdc390c7e02fb5ae2012f579ba66fbe1510d28"
+      url "https://github.com/pgsty/mc/releases/download/RELEASE.2026-09-16T00-00-00Z/mcli_20260916000000.0.0_darwin_arm64.tar.gz"
+      sha256 "1094ed5d7d14904cd3f2e0ce06211f59d968d11ad129520bb8ab2d4f674f6c83"
     end
     on_intel do
       # update: darwin_amd64
-      url "https://github.com/pgsty/mc/releases/download/RELEASE.2026-09-03T07-13-05Z/mcli_20260903071305.0.0_darwin_amd64.tar.gz"
-      sha256 "89a24e043ea635010b3f3e0b59f9c7cce41388d307de33a2c5dd5d78046c649d"
+      url "https://github.com/pgsty/mc/releases/download/RELEASE.2026-09-16T00-00-00Z/mcli_20260916000000.0.0_darwin_amd64.tar.gz"
+      sha256 "e0eea302b384a5ba0bcf3be1dc4a4e14213005d0fb8598482e2d88e9d61c9f20"
     end
   end
 
   on_linux do
     on_arm do
       # update: linux_arm64
-      url "https://github.com/pgsty/mc/releases/download/RELEASE.2026-09-03T07-13-05Z/mcli_20260903071305.0.0_linux_arm64.tar.gz"
-      sha256 "7962afc37c3e60e5758b19e819cb62d2f340ee655fad7b067e2ac9bc5716c2e8"
+      url "https://github.com/pgsty/mc/releases/download/RELEASE.2026-09-16T00-00-00Z/mcli_20260916000000.0.0_linux_arm64.tar.gz"
+      sha256 "b7008ca2a1bc5735b6585981c59a3640a0daa152dc789df3d1a0d0438787de82"
     end
     on_intel do
       # update: linux_amd64
-      url "https://github.com/pgsty/mc/releases/download/RELEASE.2026-09-03T07-13-05Z/mcli_20260903071305.0.0_linux_amd64.tar.gz"
-      sha256 "cd7fcd449bb6b52e2eb727431ba6975b1e5d90df011a75869020ea9ac9e2b2a8"
+      url "https://github.com/pgsty/mc/releases/download/RELEASE.2026-09-16T00-00-00Z/mcli_20260916000000.0.0_linux_amd64.tar.gz"
+      sha256 "4ba2814fd5507fbe6b4d237c359750b9119d28d7217495fa5b48002fcbd397ef"
     end
   end
 
